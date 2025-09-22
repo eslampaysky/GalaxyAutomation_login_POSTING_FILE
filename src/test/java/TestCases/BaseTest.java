8f8727804089f70febfe36c1a0286b8a05d5c9c7
eslampaysky <eslamsamy@payky.io>
2025-09-22 22:56:57 +0300

    Add GitHub Actions CI/CD workflow

--- src/test/java/TestCases/BaseTest.java
+++ src/test/java/TestCases/BaseTest.java
@@ -10,21 +10,23 @@ public class BaseTest {
 
     protected LoginPage loginPage;
     protected boolean loginRequired = true;
-    protected UserRole userRole = UserRole.MERCHANT; // Default
+    protected UserRole userRole = UserRole.MERCHANT; // default role
 
     @BeforeMethod
     public void setUp() {
         if (loginRequired) {
-            loginPage = new LoginPage();
+            loginPage = new LoginPage(); // Driver is handled inside LoginPage
 
+            // Login based on role
             switch (userRole) {
-                case ADMIN:
-                    loginPage.Login_admin();
-                    break;
-                case MERCHANT:
-                    loginPage.Login_merchant();
-                    break;
-            }
+                    case ADMIN:
+                        loginPage.Login_admin();
+                        break;
+                    case MERCHANT:
+                        loginPage.Login_merchant();
+                        break;
+                }
+
         }
     }
 
--- src/test/java/TestCases/LoginTest.java
+++ src/test/java/TestCases/LoginTest.java
@@ -1,29 +1,31 @@
 package TestCases;
 
-
 import io.qameta.allure.*;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-
+import Pages.LoginPage;
 
 @Epic("Authentication")
 @Feature("Login Functionality")
-public class LoginTest extends BaseTest {  // Extends BaseTest (login logic is reused)
+public class LoginTest extends BaseTest {
 
-    @Test(description = "Verify user can login with valid credentials.")
+    @Test(groups = {"critical"})
     @Severity(SeverityLevel.BLOCKER)
     @Story("User logs in with correct credentials")
-    @Description("This test checks if a user can successfully log in and sends otp successfully.")
-    public void loginWithValidData() {
-        Allure.step("Check if OTP is visible in login page after login");
-         loginPage.Login_admin();
-
-        WebElement OTP = loginPage.successMsg();  // Provided by BaseTest
-
-        Allure.step("Assert dashboard is displayed");
-        Assert.assertTrue(OTP.isDisplayed(), "Dashboard element should be visible.");
+    @Description("This test checks if a user can successfully log in and OTP is visible.")
+public void loginWithValidData() {
+    if (loginPage == null) {
+        loginPage = new LoginPage();
+        loginPage.Login_merchant();
     }
+
+    Allure.step("Check if OTP is visible in login page after login");
+    WebElement OTP = loginPage.successMsg();
+
+    Allure.step("Assert dashboard is displayed");
+    Assert.assertTrue(OTP.isDisplayed(), "Dashboard element should be visible.");
+}
+
 }
